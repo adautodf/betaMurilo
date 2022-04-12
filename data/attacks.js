0ae71d84a0b6b90b5bf073ae31cf6a7c611d0fb6
Pedro L. Barreto <plsb802@gmail.com>
2022-04-12 09:42:14 -0300

    battle changes

---
+++ data/attacks.js
@@ -0,0 +1,14 @@
+const attacks = {
+  Tackle: {
+    name: 'Tackle',
+    damage: 10,
+    type: 'D.R.',
+    color: 'red'
+  },
+  Fireball: {
+    name: 'Fireball',
+    damage: 25,
+    type: 'Orar',
+    color: 'green'
+  }
+}
